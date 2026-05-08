644ad8b731ee826339ce956ccc6925cc3457f826
Trần Đào Quốc Huy <huyt02731@gmail.com>
2026-05-08 19:01:57 +0700

    Nop bai tap 02 ca nhan
    Comment out the GET /login route in authRoutes.js

--- Login/src/routes/authRoutes.js
+++ Login/src/routes/authRoutes.js
@@ -7,7 +7,7 @@ const { validateLogin } = require("../middleware/validator");
 // POST /api/auth/login
 // Thứ tự thực thi: Rate Limiter -> Validator -> Controller
 const initAuthRoute = (app) => {
-  router.get("/login", authController.getLogin);
+  // router.get("/login", authController.getLogin);
   router.post("/login", loginLimiter, validateLogin, authController.login);
   return app.use("/api/auth", router);
 };
