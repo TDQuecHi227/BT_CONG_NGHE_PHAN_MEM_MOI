7acca974d548ef43038e817cf03ff642e92d739c
TDQuecHi <huyt02731@gmail.com>
2026-05-08 19:12:08 +0700

    Nop bai tap 02 ca nhan

--- Login/src/controllers/profileController.js
+++ Login/src/controllers/profileController.js
@@ -1,10 +1,15 @@
 const userProfile = (req, res) => {
-  res.send("This is user profile");
+  return res.status(200).json({
+    message: "This is user profile",
+    data: { userId: req.user.id, role: req.user.role },
+  });
 };
 const adminProfile = (req, res) => {
-  res.send("This is admin profile");
+  return res.status(200).json({
+    message: "This is admin profile",
+    data: { userId: req.user.id, role: req.user.role },
+  });
 };
-
 module.exports = {
   userProfile,
   adminProfile,
