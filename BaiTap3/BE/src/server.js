4bbac27f0e44529a4d67b8e94e2385b4b85b1e0b
TDQuecHi <huyt02731@gmail.com>
2026-05-11 15:09:06 +0700

    Nop bai 11/05/2026

---
+++ BaiTap3/BE/src/server.js
@@ -0,0 +1,30 @@
+require("dotenv").config;
+const express = require("express");
+const configViewEngine = require("./config/viewEngine");
+const apiRoutes = require("./routes/api");
+const connection = require("./config/configdb");
+const { getHomepage } = require("./controllers/homeController");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 8081;
+app.use(cors);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const webAPI = express.Router();
+webAPI.get("/", getHomepage);
+app.use("/", webAPI);
+
+app.use("/v1/api", apiRoutes);
+
+(async () => {
+  try {
+    await connection();
+    app.listen(port, () => {
+      console.log(`Backend Nodejs APP at : http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.log("Error connect to DB: ", error);
+  }
+})();
